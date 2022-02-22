4a74b57e676a7cb2cbee01996d2f71cec4179246
Mauro Visintin <visintin1.mauro@gmail.com>
2022-02-22 09:48:03 +0100

    base api

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 
 
-const BASE_API_URL = !__DEV__ ? "http://localhost:3000" : "https://limitless-mesa-37288.herokuapp.com"
+const BASE_API_URL = __DEV__ ? "http://localhost:3000" : "https://limitless-mesa-37288.herokuapp.com"
 
 export const getAllProblems = () =>
 fetch(`${BASE_API_URL}/api/problems`).then((res) => res.json())
